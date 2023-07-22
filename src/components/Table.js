42d71381e4388d5264a6917a9d3be25b06092ec6
effab <abeffan@gmail.com>
2023-07-22 17:16:31 -0500

    table

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,13 +3,23 @@ const Table = ({ sat }) => {
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+          {sat.map((id, data) => {
+            return (
+              <tr className={id.operational === true ? "Active" : "Inactive"} key={data}>
+                <td>{id.name}</td>
+                <td>{id.type}</td>
+                <td>{id.launchDate}</td>
+                <td>{id.operational}</td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
   );
