d97360e9a9b8bcd164fa03fbc70ed2735aba3fd8
effab <abeffan@gmail.com>
2023-07-24 17:02:53 -0500

    styling done

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,8 +1,9 @@
 import satData from "./satData";
+import './styling.css';
 
 const Buttons = (props, setSat) => {
     return (
-    <div>
+    <div className="flex-container">
       {props.displaySats.map((sat, id) => {
         return (
           <button onClick={() => props.filterByType(sat)} key={id}>{sat} Orbit</button>
