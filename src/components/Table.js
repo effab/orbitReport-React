1ea8729c6632ff1385dba6e892e8e83f3a80498b
effab <abeffan@gmail.com>
2023-07-24 16:58:51 -0500

    Table Done!

--- src/components/Table.js
+++ src/components/Table.js
@@ -12,11 +12,11 @@ const Table = ({ sat }) => {
         <tbody>
           {sat.map((id, data) => {
             return (
-              <tr className={id.operational === true ? "Active" : "Inactive"} key={data}>
+              <tr key={data}>
                 <td>{id.name}</td>
                 <td>{id.type}</td>
                 <td>{id.launchDate}</td>
-                <td>{id.operational}</td>
+                <td>{id.operational === true ? "Active" : "Inactive"}</td>
               </tr>
             )
           })}
